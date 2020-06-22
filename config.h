a177e5b11987ae75fed7395245638b10a0efd96b
Matthew Camp <usmcamp0811@gmail.com>
2020-06-22 12:05:47 -0500

    enable color emojis.. requires libxft-bgra

--- config.h
+++ config.h
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
 	{ "Gimp",      NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	{ "Alacritty", NULL,       NULL,       	    0,            0,           1,         0,        -1 },
+        { NULL,        NULL,       "Microsoft Teams Notification",       	    0,            1,           0,         0,        -1 },
 	{ NULL,        NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	/* { NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 }, */
 	/* { NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 }, */
