859615d10e8a6bddd50be122ad07ade51772741e
Matthew Camp <usmcamp0811@gmail.com>
2020-06-21 22:21:03 -0500

    compiles now

--- config.h
+++ config.h
@@ -23,11 +23,6 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-<<<<<<< HEAD
-	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
 	{ "Gimp",      NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	{ "alacritty", NULL,       NULL,       	    0,            0,           1,         0,        -1 },
