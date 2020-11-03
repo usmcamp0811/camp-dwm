5db7774cb663d1c60c9c7840c86601f0b0a867f1
Matthew Camp <usmcamp0811@gmail.com>
2020-11-03 12:42:21 -0600

    made julia plots not get swallowed

--- config.h
+++ config.h
@@ -53,6 +53,8 @@ static const Rule rules[] = {
         { "Yad",        NULL,       NULL,       	    0,            1,           0,         0,        -1 },
         { "Pavucontrol",        NULL,       NULL,       	    0,            1,           0,         0,        -1 },
 	{ NULL,        NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
+        /* julia plot */  
+	{ "gksqt", NULL,       NULL,       	    0,            0,           1,         1,        -1 },
 	/* { NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 }, */
 	/* { NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 }, */
 };
